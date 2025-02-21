4d20c3ee45f2cc1d7a20cd1c3e266c2afc71424d
ivonnenabangala <ivonnenabangala@gmail.com>
2025-02-21 18:08:40 +0300

    deploy

---
+++ .github/actions/deploy/app.js
@@ -0,0 +1,18 @@
+const core= require('@actions/core') 
+const exec= require('@actions/exec')
+
+function run(){
+
+    const bucketName = core.getInput('bucketName', {required:true})
+    const bucketRegion = core.getInput('bucketRegion', {required:true})
+    const distFiles = core.getInput('distFiles', {required:true})
+
+    // upload
+    const s3Url= `s3://${bucketName}`
+    exec.exec(` aws s3 sync ${distFiles} ${s3Url} --region ${bucketRegion}`)
+    
+   const websiteURL = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+   core.setOutput("URL", websiteURL)
+}
+
+run()
